eed3952047e258307b485a08e3744a221df09cae
Bor Hodošček <bor.hodoscek@gmail.com>
2014-09-01 11:15:41 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 
-                 [com.datomic/datomic-free "0.9.4815.12" :exclusions [org.codehaus.plexus/plexus-utils org.apache.httpcomponents/httpclient org.clojure/tools.cli com.fasterxml.jackson.core/jackson-core]]
+                 [com.datomic/datomic-free "0.9.4894" :exclusions [org.codehaus.plexus/plexus-utils org.apache.httpcomponents/httpclient org.clojure/tools.cli com.fasterxml.jackson.core/jackson-core]]
 
                  [clj-mecab "0.4.1"]
                  [corpus-utils "0.1.3"]
@@ -20,30 +20,30 @@
 
                  [prismatic/schema "0.2.6"]
                  [prismatic/plumbing "0.3.3"]
-                 [org.clojure/core.cache "0.6.3"] ;; Some package has this as an unspecified dependency
+                 [org.clojure/core.cache "0.6.4"] ;; Some package has this as an unspecified dependency
                  [com.taoensso/timbre "3.2.1"]
 
                  ;; [metosin/ring-swagger "0.8.8"]
                  ;; [metosin/ring-swagger-ui "2.0.16-2"]
-                 [ring/ring "1.3.0"]
-                 [ring/ring-core "1.3.0"]
-                 [ring/ring-devel "1.3.0"]
+                 [ring/ring "1.3.1"]
+                 [ring/ring-core "1.3.1"]
+                 [ring/ring-devel "1.3.1"]
                  [ring/ring-json "0.3.1"]
                  [ring/ring-anti-forgery "1.0.0"]
                  [commons-codec/commons-codec "1.9"]
                  [compojure "1.1.8"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.1.19"]
                  [com.taoensso/sente "0.15.1"] ;; TODO client<>server communication layer (https://github.com/ptaoussanis/sente)
                  [hiccup "1.0.5"]
                  [garden "1.2.1"]
-                 [org.clojure/core.match "0.2.1"]
+                 [org.clojure/core.match "0.2.2"]
                  ;; TODO use dire for logging: https://gist.github.com/MichaelDrogalis/3d4fb0928b2566ddc02b#file-gistfile1-clj
 
-                 [org.clojure/clojurescript "0.0-2280" :exclusions [com.google.guava/guava]]
+                 [org.clojure/clojurescript "0.0-2322" :exclusions [com.google.guava/guava]]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [om "0.7.0"]
-                 [prismatic/om-tools "0.2.2"]
-                 [sablono "0.2.20"]
+                 [om "0.7.1"]
+                 [prismatic/om-tools "0.3.3"]
+                 [sablono "0.2.22"]
                  [net.drib/strokes "0.5.1"]
 
                  ;;[compliment "0.1.0"] ;; FIXME
