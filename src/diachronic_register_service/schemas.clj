81129826da49dc709922798a298d0e02280e3388
Bor Hodošček <bor.hodoscek@gmail.com>
2015-06-29 05:17:18 +0900

    schema tweaks

--- src/diachronic_register_service/schemas.clj
+++ src/diachronic_register_service/schemas.clj
@@ -9,8 +9,10 @@
   (:import [clojure.lang PersistentQueue PersistentHashSet]
            [fast_zip.core ZipperLocation]))
 
+(def opt s/optional-key)
+
 (s/defschema CorpusOptions
-  {(s/optional-key :metadata-dir) s/Str
+  {(opt :metadata-dir) s/Str
    :corpus-dir s/Str
    :metadata-keys s/Any})
 
@@ -20,34 +22,34 @@
 
 (s/defschema MetadataMap
   "Modified MetadataSchema from corpus-utils that only contains optional keys, some of which can also be nil(?)."
-  {(s/optional-key :gender)   (s/enum :male :female :mixed)
-   (s/optional-key :audience) s/Str
-   (s/optional-key :media)    s/Str
-   (s/optional-key :topic)    s/Str
-   (s/optional-key :target-audience)  (s/enum 1 2 3 4 5)
-   (s/optional-key :hard-soft)        (s/enum 1 2 3 4)
-   (s/optional-key :informality)      (s/enum 1 2 3)
-   (s/optional-key :addressing)       (s/enum 1 2 3)
-   (s/optional-key :other-addressing) (s/enum 1 2 3 4)
+  {(opt :gender)   (s/enum :male :female :mixed)
+   (opt :audience) s/Str
+   (opt :media)    s/Str
+   (opt :topic)    s/Str
+   (opt :target-audience)  (s/enum 1 2 3 4 5)
+   (opt :hard-soft)        (s/enum 1 2 3 4)
+   (opt :informality)      (s/enum 1 2 3)
+   (opt :addressing)       (s/enum 1 2 3)
+   (opt :other-addressing) (s/enum 1 2 3 4)
    :category [s/Str]}
   #_MetadataSchema)
 
 (s/defschema StatMap
   {:df StringNumberMap
    :tf StringNumberMap
-   (s/optional-key :tf-idf) StringNumberMap ;; FIXME
-   (s/optional-key :document-count) s/Num
-   (s/optional-key :documents) PersistentHashSet
-   (s/optional-key :vocab) PersistentHashSet
-   (s/optional-key :token-count) s/Num})
+   (opt :tf-idf) StringNumberMap ;; FIXME
+   (opt :document-count) s/Num
+   (opt :documents) PersistentHashSet
+   (opt :vocab) PersistentHashSet
+   (opt :token-count) s/Num})
 
 (s/defschema Metadata->StatMap
   {MetadataMap StatMap})
 
 (s/defschema Document ;; FIXME: naming "Schema"
   (merge DocumentSchema
-         {(s/optional-key :token-count) s/Num
-          (s/optional-key :token-freqs) StringNumberMap}))
+         {(opt :token-count) s/Num
+          (opt :token-freqs) StringNumberMap}))
 
 (s/defschema ModelSchema
   {;;:type (s/enum :global :subgroup)
@@ -62,18 +64,21 @@
 (s/defschema MetadataPath
   [MetadataPart])
 
+(s/defschema Facet ;; (s/enum [(s/enum s/Str s/Num)] s/Str s/Num)
+  {s/Keyword s/Any})
+
 ;; TODO use defmodel to specify API schemas.
 (comment
   (defmodel APIQuery
     "API word query schema"
-    {(s/optional-key :lemma) s/Str
-     (s/optional-key :pos) s/Str
+    {(opt :lemma) s/Str
+     (opt :pos) s/Str
      :orth-base s/Str})
 
   (defmodel DBQuery
     "DB word query schema"
-    {(s/optional-key :lemma) s/Str
-     (s/optional-key :pos) s/Str
+    {(opt :lemma) s/Str
+     (opt :pos) s/Str
      :orth-base s/Str})
 
   (comment
